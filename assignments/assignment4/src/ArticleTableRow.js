c83c4d327e6c697dc71945e7ac6c4fb6a2b0a4d5
jenniferyuexiao <46546080+jenniferyuexiao@users.noreply.github.com>
2019-03-04 04:14:34 -0800

    Add files via upload

---
+++ assignments/assignment4/src/ArticleTableRow.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ArticleTableRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.title,
+      author: this.props.author,
+      date: this.props.date,
+      shortText: this.props.shortText,
+      selected: false
+    };
+
+  }
+
+  onClick() {
+    this.setState({
+      selected: !this.state.selected
+    });
+  }
+
+  render() {
+
+    let selectedStatus;
+    if (this.state.selected) {
+      selectedStatus = 'Yes';
+    }
+    else {
+      selectedStatus = 'No';
+    }
+
+    // paste this onClick handler into your checkbox;
+    // you should not have to modify this code in any way
+    // onClick={this.onClick.bind(this)}
+    //
+    return (
+      <tr align="left">
+      <td align="left"> <input type="checkbox" onClick= {this.onClick.bind(this)}/> {this.props.title} </td>
+      <td> {selectedStatus} </td>
+      <td> <address> <a rel={this.author} href= "/author"> {this.props.author} </a> </address> </td>
+      <td> <time dateTime= "2018">2018</time> </td>
+      <td> {this.props.shortText} </td>
+      </tr>
+    );
+  }
+
+}
+
+ArticleTableRow.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  shortText: PropTypes.string.isRequired
+};
+
+export default ArticleTableRow;
